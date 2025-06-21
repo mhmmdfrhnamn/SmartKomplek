f7147def27e2261fce6b80b3f91431d9ebe842a6
Muhammad Farhan Amien <farhanexcellentz17@gmail.com>
2025-06-21 14:19:37 +0700

    Menambahkan halaman awal AddAnnouncementScreen (form buat pengumuman)

---
+++ lib/presentation/home/screens/add_announcement_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddAnnouncementScreem extends StatelessWidget {
+  const AddAnnouncementScreem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Buat Pengumuman Baru")),
+      body: const Center(child: Text("Form untuk membuat pengumuman akan ada disini")),
+    );
+  }
+}
